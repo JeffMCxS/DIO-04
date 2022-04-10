85084aa1fb77460a98a2833754222f3276ea62a6
Jeff <aurelio.sg3@gmail.com>
2022-04-10 19:01:21 -0300

    fim da aula 05 - tratamento da excecao try catch

--- php-oo/excecoes.php
+++ php-oo/excecoes.php
@@ -2,7 +2,7 @@
 
 throw new Exception("Essa e uma exceção!");
 
-echo "\n ... executando ...\n"; //Essa linha não será executada pois o Exception interrompe o código.
+echo "\n ... executando ...\n"; // Essa linha não será executada pois o Exception interrompe o código.
 
 //
 
@@ -20,6 +20,16 @@ $usuario = [
     'idade' => 57,
 ];
 
-validarUsuario($usuario);
+// Tratando exceções com Try Catch
+// Será feita uma tentativa de validar o usuário, se retornar uma excessão,
+// ela é capturada (tratada) pelo Catch, caso contrário o código continua sua execução.
+try {
+    $status = validarUsuario($usuario);
+} catch (Exception $e) {
+    echo $e -> getMessage(); //O $e recebe a exceção da linha 12
+    //die(); // Interrompe o código
+} finally { // Finally sempre é executado independente se retornar uma exceção ou não.
+    echo "Status da Operação: " . (int)$status; // Cast - transformou o resultado de true/false para 1/0
+}
 
 echo "\n ... executando ...\n";
